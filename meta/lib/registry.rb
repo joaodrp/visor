68b87dd432399f306b38758b74ceb7e3d201dc72
João Pereira <joaodrp@gmail.com>
2011-12-23 15:31:09 +0000

    Improved gemspec, loader file and included version.

--- meta/lib/registry.rb
+++ meta/lib/registry.rb
@@ -1,18 +1,12 @@
-module Visor
+## Is this the best way to require another sub-system/gem in the same global project folder?
+$:.unshift File.expand_path('../../../common/lib', __FILE__)
+require 'common'
+##
+$:.unshift File.expand_path('../lib', __FILE__)
+require 'registry/version'
+require 'registry/client'
+require 'registry/api'
+require 'registry/backends/base'
+require 'registry/backends/mongo_db'
+require 'registry/backends/mysql_db'
 
-  #TODO: try Yajl
-
-  # Require libraries
-  #
-  $:.unshift(File.join(File.dirname(__FILE__), '../../common/lib'))
-
-  require 'common'
-
-  $:.unshift(File.join(File.dirname(__FILE__), '../lib'))
-
-  require 'registry/client'
-  require 'registry/api'
-  require 'registry/backends/base'
-  require 'registry/backends/mongo_db'
-  require 'registry/backends/mysql_db'
-end
---
+++ meta/lib/registry/version.rb
@@ -0,0 +1,5 @@
+module Visor
+  module Registry
+    VERSION = "0.0.1"
+  end
+end
