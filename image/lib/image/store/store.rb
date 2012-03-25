a9dca0c41d28a725bba3e8ef3b86646da93c25ae
João Pereira <joaodrp@gmail.com>
2012-03-25 19:59:54 +0100

    Added support for Lunacloud storage (beta)

--- image/lib/image/store/store.rb
+++ image/lib/image/store/store.rb
@@ -11,12 +11,13 @@ module Visor
       include Visor::Common::Exception
 
       # Base API mapping for the multiple storage backend classes
-      BACKENDS = {:s3      => Visor::Image::Store::S3,
-                  :cumulus => Visor::Image::Store::Cumulus,
-                  :walrus  => Visor::Image::Store::Walrus,
-                  :hdfs    => Visor::Image::Store::HDFS,
-                  :file    => Visor::Image::Store::FileSystem,
-                  :http    => Visor::Image::Store::HTTP}
+      BACKENDS = {:s3        => Visor::Image::Store::S3,
+                  :cumulus   => Visor::Image::Store::Cumulus,
+                  :walrus    => Visor::Image::Store::Walrus,
+                  :lunacloud => Visor::Image::Store::Lunacloud,
+                  :hdfs      => Visor::Image::Store::HDFS,
+                  :file      => Visor::Image::Store::FileSystem,
+                  :http      => Visor::Image::Store::HTTP}
 
       # Get a store backend class object ready to use, based on a file URI or store name.
       #
